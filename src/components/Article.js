3279bbe8d7ed7a101839a67a4b1967aaecacbe3d
Drew <dch528@gmail.com>
2024-02-27 11:29:28 -0500

    Drew H Putting it All Together Components and Props

---
+++ src/components/Article.js
@@ -0,0 +1,15 @@
+function Article({ title, date = "January 1, 1970", preview, minutes }) {
+    // const emojis = makeEmojiList(minutes);
+  
+    return (
+      <article>
+        <h3>{title}</h3>
+        <small>
+          {date} {minutes} min read
+        </small>
+        <p>{preview}</p>
+      </article>
+    );
+  }
+  
+  export default Article;
